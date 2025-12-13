20584e6f54f9d22216a2b1b67dfdd472a1fdf2f9
ThomasAvuni <eclissifasica@gmail.com>
2025-12-13 19:14:50 +0100

    Aggiunto input utente e decript

--- CifrarioDiCesare/main.cpp
+++ CifrarioDiCesare/main.cpp
@@ -1,29 +1,95 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include<string>
 
-int main(int argc, char* argv[])
+std::string Cifra(std::string stringa)
 {
-    std::vector<std::string> alfabeto = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z"};
-    std::vector<std::string> nome = {"C", "I", "A", "O"};
+    std::vector<std::string> alfabeto = {"A", "B", "C", "D", "E", "F", "G", "H",
+        "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+        "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     const int key = 3;
     std::vector<std::string> result;
-
-    for (int i = 0; i < nome.size(); i++)
+    std::transform(stringa.begin(), stringa.end(), stringa.begin(), toupper);
+    
+    for (int i = 0; i < stringa.length(); i++)
     {
         for (int j = 0; j < alfabeto.size(); j++)
         {
-            if (nome[i] == alfabeto[j])
+            if (stringa.c_str()[i] == *alfabeto[j].c_str())
             {
                 int newIndex = (j + key) % alfabeto.size();
                 result.push_back(alfabeto[newIndex]);
             }
         }
     }
+
+    std:: string s;
+    for (int i = 0; i < result.size(); i++)
+    {
+        s.append(result[i]);
+    }
+    return s;
+}
+
+std::string Decripta(std::string stringa)
+{
+    std::vector<std::string> alfabeto = {"A", "B", "C", "D", "E", "F", "G", "H",
+        "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+        "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    const int key = 3;
+    std::vector<std::string> result;
+    std::transform(stringa.begin(), stringa.end(), stringa.begin(), toupper);
     
-    std::cout<<key<< "\n";
-    for (std::string s : result)
-        std::cout<<s<<", ";
-    
+    for (int i = 0; i < stringa.length(); i++)
+    {
+        for (int j = 0; j < alfabeto.size(); j++)
+        {
+            if (stringa.c_str()[i] == *alfabeto[j].c_str())
+            {
+                int newIndex = (j - key) % alfabeto.size();
+                result.push_back(alfabeto[newIndex]);
+            }
+        }
+    }
+
+    std:: string s;
+    for (int i = 0; i < result.size(); i++)
+    {
+        s.append(result[i]);
+    }
+    return s;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"|Benvenuto nel Cifrario di Cesare|\n";
+    std::cout<<"|Vuoi cifrare (1) oppure decriptare (2) qualcosa?|\n";
+    int r = 0;
+    std::cin>>r;
+    std::cin.ignore(1000, '\n');
+    switch (r)
+    {
+    case 1:
+        {
+            std::cout<<"|Inserisci la frase che vuoi Cifrare|\n";
+            std::string s;
+            std::getline(std::cin, s);
+            std::cout<<Cifra(s);
+        }
+        break;
+    case 2:
+        {
+            std::cout<<"|Inserisci la frase che vuoi Decriptare|\n";
+            std::string s;
+            std::getline(std::cin, s);
+            std::cout<<Decripta(s);
+        }
+        break;
+    default:
+        std::cout<<"Operazione non valida";
+        break;
+    }
+
     return 0;
 }
